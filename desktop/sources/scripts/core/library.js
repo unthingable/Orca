087840d5d89a6457283f1e10007228865a857c0a
unthingable <unthingable@users.noreply.github.com>
2020-09-28 10:46:06 +0200

    duration-based Z

--- desktop/sources/scripts/core/library.js
+++ desktop/sources/scripts/core/library.js
@@ -457,16 +457,28 @@ library.z = function OperatorZ (orca, x, y, passive) {
   this.name = 'lerp'
   this.info = 'Transitions operand to target'
 
-  this.ports.rate = { x: -1, y: 0, default: '1' }
+  this.ports.stepped = { x: -2, y: 0, default: '0' }
+  this.ports.duration = { x: -1, y: 0, default: '8' }
   this.ports.target = { x: 1, y: 0 }
   this.ports.output = { x: 0, y: 1, sensitive: true, reader: true, output: true }
 
   this.operation = function (force = false) {
-    const rate = this.listen(this.ports.rate, true)
+    const duration = this.listen(this.ports.duration, true)
     const target = this.listen(this.ports.target, true)
     const val = this.listen(this.ports.output, true)
-    const mod = val <= target - rate ? rate : val >= target + rate ? -rate : target - val
-    return orca.keyOf(val + mod)
+    var stepped = this.listen(this.ports.stepped, true)
+    if (stepped >= duration) {
+      stepped = duration
+      this.output(stepped, this.ports.stepped)
+    }
+    if (val !== target) {
+      if (stepped === duration) { stepped = 0 }  // previous iteration done, ok to restart
+      const steps = duration - stepped
+      const remaining = target - val
+      const mod = Math.round(remaining / steps)
+      this.output(orca.keyOf(val + mod), this.ports.output)
+      this.output(orca.keyOf(stepped + 1), this.ports.stepped)
+    }
   }
 }
 
